81905287b37567d5e5cfb3fd2f377d3f0c33342c
Antonio Antillon <antillas21@gmail.com>
2013-01-02 19:51:31 -0800

    initial repo setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AndreBadi::Application.config.secret_token = 'e214409205fb5157ad61069cc8ae5ca8cbd38a3ac23ac789d0af87cf0d2ef82fdb63528b0683ad37cfca9b17f01d1a2c3b4a4ef0af455b2e78128861672e89ae'
