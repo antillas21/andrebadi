5b5fd738a6606b5f1fe72d192061cc79623fe20a
Antonio Antillon <antillas21@gmail.com>
2013-01-03 20:24:28 -0800

    Adds specs and basic code for User model.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of :email }
+  end
+
+  describe 'relationships' do
+    it { should have_many :customers }
+  end
 end
