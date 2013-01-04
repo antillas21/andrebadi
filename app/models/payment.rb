0766a3b829bf46975278cb545edf9b09a92f7ebc
Antonio Antillon <antillas21@gmail.com>
2013-01-03 20:23:21 -0800

    Adds basic specs and code for Payment model.

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,3 +1,7 @@
 class Payment < ActiveRecord::Base
   # attr_accessible :title, :body
+
+  validates :payment_date, :amount, presence: true
+
+  belongs_to :customer
 end
--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe Payment do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of :payment_date }
+    it { should validate_presence_of :amount }
+  end
+
+  describe 'relationships' do
+    it { should belong_to :customer }
+  end
 end
