babddccf4b7d0e04cf0a65fecc55ec4a6073da70
Antonio Antillon <antillas21@gmail.com>
2013-02-25 22:21:29 -0800

    Adds empty Api::PaymentsController.

---
+++ spec/controllers/api/payments_controller_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe Api::PaymentsController do
+
+  describe 'GET #index' do
+    before :each do
+    end
+
+    it 'should be success'
+    it 'retrieves all payments from a customer'
+  end
+
+  describe 'POST #create' do
+  end
+
+  describe 'GET #show' do
+  end
+
+  describe 'PUT #update' do
+  end
+
+  describe 'DELETE #destroy' do
+  end
+
+end
