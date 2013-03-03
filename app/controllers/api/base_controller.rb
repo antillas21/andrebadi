45119831704f68df2e69e5d1e68ffd27492f88bc
Antonio Antillon <antillas21@gmail.com>
2013-03-03 09:57:51 -0800

    Prepends method to Api::BaseController to allow passing auth token in request headers.

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -3,8 +3,15 @@ class Api::BaseController < ActionController::Base
   respond_to :json
 
   before_filter :authenticate_user
+  prepend_before_filter :get_auth_token
 
   private
+  def get_auth_token
+    if auth_token = params[:token].blank? && request.headers["X-AUTH-TOKEN"]
+      params[:token] = auth_token
+    end
+  end
+
   def authenticate_user
     @logged_user = User.where(authentication_token: params[:token]).first
     respond_with({ error: "Token is missing or is invalid." })  unless @logged_user
