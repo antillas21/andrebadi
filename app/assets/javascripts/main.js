aada6c95ea09ff50a766aad5080953d1f75d4cb1
Antonio Antillon <antillas21@gmail.com>
2013-03-03 09:58:26 -0800

    Temporarily comments out code in this main.js.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,47 +1,47 @@
-    var HomeView = Backbone.View.extend({
-        template: _.template($('#home').html()),
+    // var HomeView = Backbone.View.extend({
+    //     template: _.template($('#home').html()),
 
-        render: function (eventName) {
-            $(this.el).html(this.template());
-            return this;
-        }
-    });
+    //     render: function (eventName) {
+    //         $(this.el).html(this.template());
+    //         return this;
+    //     }
+    // });
 
-    var AppRouter = Backbone.Router.extend({
-        routes: { "": "home" },
+    // var AppRouter = Backbone.Router.extend({
+    //     routes: { "": "home" },
 
-        initialize: function () {
-            $('.back').on('click', function(event) {
-                window.history.back();
-                return false;
-            });
+    //     initialize: function () {
+    //         $('.back').on('click', function(event) {
+    //             window.history.back();
+    //             return false;
+    //         });
 
-            this.firstPage = true;
-        },
+    //         this.firstPage = true;
+    //     },
 
-        home: function () {
-            console.log('#home');
-            this.changePage(new HomeView());
-        },
+    //     home: function () {
+    //         console.log('#home');
+    //         this.changePage(new HomeView());
+    //     },
 
-        changePage: function (page) {
-            $(page.el).attr('data-role', 'page');
-            page.render();
-            $('body').append($(page.el));
-            var transition = $.mobile.defaultPageTransition;
-            // We don't want to slide the first page
-            if (this.firstPage) {
-                transition = 'none';
-                this.firstPage = false;
-            }
-            $.mobile.changePage($(page.el), {changeHash:false, transition: transition});
-        }
+    //     changePage: function (page) {
+    //         $(page.el).attr('data-role', 'page');
+    //         page.render();
+    //         $('body').append($(page.el));
+    //         var transition = $.mobile.defaultPageTransition;
+    //         // We don't want to slide the first page
+    //         if (this.firstPage) {
+    //             transition = 'none';
+    //             this.firstPage = false;
+    //         }
+    //         $.mobile.changePage($(page.el), {changeHash:false, transition: transition});
+    //     }
 
-    });
+    // });
 
-    $(document).ready(function () {
-        console.log('document ready');
-        app = new AppRouter();
-        Backbone.history.start();
-    });
+    // $(document).ready(function () {
+    //     console.log('document ready');
+    //     app = new AppRouter();
+    //     Backbone.history.start();
+    // });
 
