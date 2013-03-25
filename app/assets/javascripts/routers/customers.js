c4a5950c02a2a447d6cc22bec7000b03468880da
Antonio Antillon <antillas21@gmail.com>
2013-03-25 00:11:43 -0700

    Cleans how initial CustomersIndex view is rendered when calling the index route on Customers Router.

--- app/assets/javascripts/routers/customers.js
+++ app/assets/javascripts/routers/customers.js
@@ -5,8 +5,6 @@ App.Routers.Customers = Backbone.Router.extend({
 
   index: function() {
     var view = new App.Views.CustomersIndex({ collection: App.customers });
-    view.render()
-    $('#backbone-container').html(view.el);
-    // $('#backbone-container').html(view.render().$el);
+    $('#backbone-container').html(view.render().$el);
   }
 });
