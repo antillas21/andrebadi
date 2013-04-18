3a217c1f3ca23ab1bbd6b9795049101efc7f71fc
Antonio Antillon <antillas21@gmail.com>
2013-04-17 21:15:35 -0700

    Adds recentPayments attr to CustomerFullView.

--- app/assets/javascripts/views/customer_fullview.js
+++ app/assets/javascripts/views/customer_fullview.js
@@ -1,6 +1,7 @@
 App.Views.CustomerFullView = Backbone.View.extend({
   initialize: function() {
     this.model.on('change', this.render, this);
+    this.model.recentPayments = this.model.get('recent_payments');
   },
   
   render: function() {
