803a434341765c967ff3a6ce79ad40762e6f395b
Antonio Antillon <antillas21@gmail.com>
2013-07-17 17:52:22 -0700

    Updates serializer for Sale.

--- app/serializers/sale_serializer.rb
+++ app/serializers/sale_serializer.rb
@@ -1,4 +1,4 @@
-class PurchaseSerializer < ActiveModel::Serializer
+class SaleSerializer < ActiveModel::Serializer
   attributes :id, :uid, :type, :amount, :cost, :created_at, :updated_at
   has_one :customer, serializer: CustomerListSerializer
   has_many :line_items
